93cc9d260d634189c58c4840caf03c8077872bf2
Tushar Goel <tushar.goel.ml@gmail.com>
2022-10-12 12:41:28 +0530

    Model recompiling done

--- DarkNeurons/Model_Training.py
+++ DarkNeurons/Model_Training.py
@@ -129,7 +129,7 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
     print('Create YOLOv4 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
 
     if load_pretrained:
-        model_body = load_model(weights_path)
+        model_body.load_weights(weights_path)
         print('Load weights {}.'.format(weights_path))
         if freeze_body in [1, 2]:
             # Freeze darknet53 body or freeze all but 3 output layers.
